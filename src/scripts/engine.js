2e85d12c195026f1aa4a0015e6135e0427133ada
Junn-era <junnera@gmail.com>
2024-01-03 12:03:52 -0300

    Updating files...

---
+++ src/scripts/engine.js
@@ -0,0 +1,50 @@
+const pianoKeys = document.querySelectorAll(".piano-keys .key");
+const volumeSlider = document.querySelector(".volume-slider input");
+let mapedKeys = [];
+let audio = new Audio("src/tunes/a.wav");
+
+const playTune = (key) => {
+  audio.src = `src/tunes/${key}.wav`
+  audio.play();
+
+  console.log(mapedKeys);
+  const clickedKey = document.querySelector(`[data-key="${key}"]`)
+  clickedKey.classList.add("active")
+  setTimeout(() => {
+    clickedKey.classList.remove("active");
+  }, 150)
+
+};
+
+pianoKeys.forEach((key) => {
+  //console.log(key.dataset.key);
+  key.addEventListener("click", () => playTune(key.dataset.key));
+  mapedKeys.push(key.dataset.key);
+});
+
+document.addEventListener("keydown", (e) => {
+  if (mapedKeys.includes(e.key)) {
+    playTune(e.key);
+  }
+  //console.log(e.key);
+});
+// Volume Control
+const handleVolume = (e) => {
+  audio.volume = e.target.value;
+  //console.log(e.target.value);
+};
+volumeSlider.addEventListener("input", handleVolume);
+
+
+// Function for slider percentage
+// $(function() {
+//   $(".column volume-slider").slider({
+//     range: "min",
+//     min: 0,
+//     max: 100,
+//     value: 0,
+//     slide: function(event, ui) {
+//       $("#percentage").text(ui.value + "%");
+//     }
+//   });
+// });
